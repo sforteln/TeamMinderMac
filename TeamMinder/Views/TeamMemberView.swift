02dbccd228de06ced73028d55972575116894d88
Simon Fortelny <sforteln@gmail.com>
2023-03-30 14:06:25 -0400

    refactor trend data

--- TeamMinder/Views/TeamMemberView.swift
+++ TeamMinder/Views/TeamMemberView.swift
@@ -11,27 +11,6 @@ struct TeamMemberView: View {
     @EnvironmentObject var team: Team
     @ObservedObject var member: TeamMember
     var body: some View {
-        var points: [TrendDataPoint] = [
-            TrendDataPoint(level: 2, date: daysAgo(12)),
-            TrendDataPoint(level: 3, date: daysAgo(22)),
-            TrendDataPoint(level: 1, date: daysAgo(23)),
-            TrendDataPoint(level: 1, date: daysAgo(30)),
-            TrendDataPoint(level: 5, date: daysAgo(33)),
-            TrendDataPoint(level: 4, date: daysAgo(43)),
-            TrendDataPoint(level: 5, date: daysAgo(47)),
-            TrendDataPoint(level: 5, date: daysAgo(53)),
-            TrendDataPoint(level: 3, date: daysAgo(60)),
-            TrendDataPoint(level: 3, date: daysAgo(67)),
-            TrendDataPoint(level: 2, date: daysAgo(73)),
-            TrendDataPoint(level: 1, date: daysAgo(80)),
-            TrendDataPoint(level: 4, date: daysAgo(87)),
-            TrendDataPoint(level: 1, date: daysAgo(93)),
-            TrendDataPoint(level: 3, date: daysAgo(100))
-        ]
-        let trendData1 = TrendData(points: points, title: "How stessed")
-        let trendData2 = TrendData(points: points, title: "How burned out")
-        let trendData3 = TrendData(points: points, title: "How happy")
-        
         TabView {
             InfoView(member: member)
             .tabItem {
@@ -45,7 +24,7 @@ struct TeamMemberView: View {
             .tabItem {
                 Label("Completed Questions", systemImage: "questionmark.bubble")
             }
-            TrendsView(data: [trendData1, trendData2, trendData3])
+            TrendsView(data: testTrendData())
             .tabItem {
                 Label("Trends", systemImage: "questionmark.bubble")
             }
--- TeamMinder/Views/TrendsView.swift
+++ TeamMinder/Views/TrendsView.swift
@@ -8,26 +8,9 @@
 import SwiftUI
 import Charts
 
-struct TrendDataPoint: Identifiable {
-    var level: Int
-    var date: Date
-    var id = UUID()
-}
 
-struct TrendData: Identifiable {
-    var id = UUID()
-    var points : [TrendDataPoint]
-    var title : String
-    
-    func addNewPoint(level: Int) {
-        
-    }
 
-}
 
-func daysAgo(_ ago: Int) -> Date {
-    return Date(timeIntervalSinceNow: TimeInterval(ago * -86400))
-}
 
 struct TrendsView: View {
     var data : [TrendData]
@@ -92,27 +75,8 @@ struct EmojiButton: View {
 
 struct TrendsView_Previews: PreviewProvider {
     static var previews: some View {
-        var points: [TrendDataPoint] = [
-            TrendDataPoint(level: 2, date: daysAgo(12)),
-            TrendDataPoint(level: 3, date: daysAgo(22)),
-            TrendDataPoint(level: 1, date: daysAgo(23)),
-            TrendDataPoint(level: 1, date: daysAgo(30)),
-            TrendDataPoint(level: 5, date: daysAgo(33)),
-            TrendDataPoint(level: 4, date: daysAgo(43)),
-            TrendDataPoint(level: 5, date: daysAgo(47)),
-            TrendDataPoint(level: 5, date: daysAgo(53)),
-            TrendDataPoint(level: 3, date: daysAgo(60)),
-            TrendDataPoint(level: 3, date: daysAgo(67)),
-            TrendDataPoint(level: 2, date: daysAgo(73)),
-            TrendDataPoint(level: 1, date: daysAgo(80)),
-            TrendDataPoint(level: 4, date: daysAgo(87)),
-            TrendDataPoint(level: 1, date: daysAgo(93)),
-            TrendDataPoint(level: 3, date: daysAgo(100))
-        ]
-        let trendData1 = TrendData(points: points, title: "How stessed")
-        let trendData2 = TrendData(points: points, title: "How burned out")
-        let trendData3 = TrendData(points: points, title: "How happy")
-        TrendsView(data: [trendData1, trendData2, trendData3])
+
+        TrendsView(data: testTrendData())
     }
 }
 
